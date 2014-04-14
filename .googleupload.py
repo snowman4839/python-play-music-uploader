712911b700fd5b25758457d7525e12c36c185fec
snowman4839 <snowman4839@comcast.net>
2014-04-14 12:34:31 -0500

    Fixes artwork upload via eyeD3 (adds eyeD3 dependency)

--- .googleupload.py
+++ .googleupload.py
@@ -4,22 +4,28 @@
 from getpass import getpass
 
 from gmusicapi import Musicmanager
+from gmusicapi import Webclient
 
 import os
 import time
 
 from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler, FileCreatedEvent
+from watchdog.events import PatternMatchingEventHandler, FileSystemEventHandler, FileCreatedEvent
 
 from subprocess import call
 
 import glob
 
-**************************************
+######################################
 oauthFile = "/home/owner/.googleoauth"
-uploadDir = "/mnt/hd0/Music/uploads/"
-storageDir = "/mnt/hd0/Music/"
-**************************************
+uploadDir = "/mnt/hd0/Music/Android Music/uploads/"
+storageDir = "/mnt/hd0/Music/Android Music/"
+#####################################
+
+####################################
+googleEmail = ""
+googlePass = ""
+####################################
 
 def ask_for_credentials():
 	"""Make an instance of the api and attempts to login with it.
@@ -39,6 +45,9 @@ def ask_for_credentials():
 def upload():
 	api = ask_for_credentials()
 
+	uploader = Webclient()
+	uploader.login(googleEmail, googlePass)
+
 	if not api.is_authenticated():
 		print "Sorry, those credentials weren't accepted."
 		return
@@ -46,45 +55,64 @@ def upload():
 	print 'Successfully logged in.'
 	print
 
+	time.sleep(10);
+
 	mp3files = []
 	for root, dirs, files in os.walk(uploadDir):
 		for file in files:
 			if file.endswith('.mp3'):
 				mp3files.append(os.path.join(root, file))
+			if file.endswith('.m4a'):
+				mp3files.append(os.path.join(root, file))
+
+	call(["mkdir","/tmp/cover"])
 
 	for each in mp3files:
-	print each
-	uploadDict = api.upload(each, 1, True)
-	if uploadDict[0]:
-		call(["mv",each,storageDir])
-		print "uploaded"
-	elif uploadDict[1]:
-		call(["mv",each,storageDir])
-		print "matched song on google servers"
-	elif uploadDict[2]:
-		call(["mv",each,storageDir])
-		print "already uploaded"
+		print each
+		uploadDict = api.upload(each, 1, True)
+		if uploadDict[0]:
+			songid = (uploadDict[0])[each]
+			call(["eyeD3","--write-images=/tmp/cover",each])
+			call(["sleep","1"])
+			cover = glob.glob("/tmp/cover/*")
+			if (coverFile):
+				print coverFile
+				call(["mv",coverFile[0],"/tmp/cover/img.jpg"])
+				uploader.upload_album_art(songid,"/tmp/cover/img.jpg")
+				#call(["rm","/tmp/cover/*"])
+			call(["mv",each,storageDir])
+			print "uploaded"
+		elif uploadDict[1]:
+			call(["mv",each,storageDir])
+			print "matched song on google servers"
+		elif uploadDict[2]:
+			call(["mv",each,storageDir])
+			print "already uploaded"
 
+	call(["rm","-R","/tmp/cover"])
+
+	print "logging out"
 	api.logout()
+	uploader.logout();
 	print "logged out"
 
-class Handler(FileSystemEventHandler):
-	def on_created(self, event):
-		print "new file!"
+class Handler(PatternMatchingEventHandler):
+	def on_any_event(self, event):
+		print "new MP3 file!"
 		upload()
 
 if __name__ == '__main__':
-	upload()
-	eventWatcher = Handler()
+	#upload()
+	eventWatcher = Handler("*.mp3",["*.mp3.tacitpart"],False,False)
 	observer = Observer()
 	observer.schedule(eventWatcher,uploadDir,recursive=False)
 	print "starting"
 	observer.start()
 	try:
-	while True:
-		time.sleep(1)
+		while True:
+			time.sleep(1)
 	except KeyboardInterrupt:
-	print "stopping"
-	observer.stop()
-	print "joining"
-	observer.join()
+		print "stopping"
+		observer.stop()
+		print "joining"
+		observer.join()
