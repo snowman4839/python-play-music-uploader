ae1528ff4f9e4e63270b710d6e1630cc34275604
snowman4839 <snowman4839@comcast.net>
2014-04-14 13:07:30 -0500

    Commented everything and fixed misnamed variable for eyeD3 upload

--- .googleupload.py
+++ .googleupload.py
@@ -43,20 +43,26 @@ def ask_for_credentials():
 
 
 def upload():
+	#Init music manager
 	api = ask_for_credentials()
 
+	#Init web client
 	uploader = Webclient()
 	uploader.login(googleEmail, googlePass)
 
+	#Present error if auth isn't accepted
 	if not api.is_authenticated():
 		print "Sorry, those credentials weren't accepted."
 		return
 
+	#Success message if auth works
 	print 'Successfully logged in.'
 	print
 
+	#Wait for any copies or uploads to watched folder to finish
 	time.sleep(10);
 
+	#Populate mp3files array with music files in the upload directory
 	mp3files = []
 	for root, dirs, files in os.walk(uploadDir):
 		for file in files:
@@ -65,52 +71,78 @@ def upload():
 			if file.endswith('.m4a'):
 				mp3files.append(os.path.join(root, file))
 
+	#Create temp cover directory for artwork extraction and upload via eyeD3
 	call(["mkdir","/tmp/cover"])
 
+	#Upload and hand each appropriate case after upload
 	for each in mp3files:
+
+		#Print and upload the current file
 		print each
 		uploadDict = api.upload(each, 1, True)
+
+		#If upload is successful
 		if uploadDict[0]:
+			#Get song id
 			songid = (uploadDict[0])[each]
+			#Extract artwork via eyeD3
 			call(["eyeD3","--write-images=/tmp/cover",each])
 			call(["sleep","1"])
-			cover = glob.glob("/tmp/cover/*")
+			#Get the new artwork path
+			coverFile = glob.glob("/tmp/cover/*")
+			#If artwork was extracted
 			if (coverFile):
+				#Print the path to the artwork
 				print coverFile
+				#Move it to a absolute location and upload it to the newly uploaded song
 				call(["mv",coverFile[0],"/tmp/cover/img.jpg"])
 				uploader.upload_album_art(songid,"/tmp/cover/img.jpg")
 				#call(["rm","/tmp/cover/*"])
+			#Move uploaded file to the storage directory
 			call(["mv",each,storageDir])
 			print "uploaded"
+		#Move uploaded file to the storage directory if it was matched to a song on the google servers
 		elif uploadDict[1]:
 			call(["mv",each,storageDir])
 			print "matched song on google servers"
+		#Move uploaded file to the storage directory if it was already uploaded to the account
 		elif uploadDict[2]:
 			call(["mv",each,storageDir])
 			print "already uploaded"
 
+	#Remove everything dealing with the temp artwork extraction
 	call(["rm","-R","/tmp/cover"])
 
+	#Logout of both interfaces
 	print "logging out"
 	api.logout()
-	uploader.logout();
+	uploader.logout()
 	print "logged out"
 
+#Class to handle events when anything happens in the watched folder
 class Handler(PatternMatchingEventHandler):
 	def on_any_event(self, event):
 		print "new MP3 file!"
 		upload()
 
 if __name__ == '__main__':
-	#upload()
+	#Initial upload on start
+	upload()
+
+	#Watch folder for newly added .mp3 files, ignore .tacitpart files while file is being uploaded
 	eventWatcher = Handler("*.mp3",["*.mp3.tacitpart"],False,False)
 	observer = Observer()
 	observer.schedule(eventWatcher,uploadDir,recursive=False)
+
+	#Begin watch
 	print "starting"
 	observer.start()
+
+	#Watch the folder continuously 
 	try:
 		while True:
 			time.sleep(1)
+	#Unless there's a keyboard interrupt
 	except KeyboardInterrupt:
 		print "stopping"
 		observer.stop()
